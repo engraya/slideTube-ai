6e455976ab9b79bfd683ee476699397e8c3cf4ad
Engraya <ahmadyakubuaya@gmail.com>
2024-11-10 19:03:04 +0100

    initial Commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,40 @@
+import '../global.css'
+
+import type { Metadata } from 'next'
+import { Plus_Jakarta_Sans as PlusJakartaSans } from 'next/font/google'
+import Analytics from '@/components/analytics'
+import Footer from '@/components/footer'
+import Header from '@/components/header'
+import { ThemeProvider } from '@/components/providers'
+import { cn } from '@/lib/utils'
+
+const fontSans = PlusJakartaSans({
+  subsets: ['latin'],
+  variable: '--font-sans',
+  weight: ['400', '500', '600', '700', '800'],
+})
+
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en" suppressHydrationWarning className={fontSans.variable}>
+      <body>
+        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+          <div
+            className={cn(
+              'fixed h-screen w-full bg-gradient-to-br from-background to-blue-50 dark:bg-gradient-to-br dark:from-gray-800 dark:via-gray-900 dark:to-slate-900',
+            )}
+          />
+          <Header />
+          <main className={cn('relative z-10')}>{children}</main>
+          <Footer />
+          <Analytics />
+        </ThemeProvider>
+      </body>
+    </html>
+  )
+}
---
+++ src/components/header/header.tsx
@@ -0,0 +1,33 @@
+'use client'
+
+import Link from 'next/link'
+import { useOnScroll } from '@/hooks'
+import { cn } from '@/lib/utils'
+
+import ThemeSwitch from '../theme-switch'
+
+const Header = () => {
+  const isScrolled = useOnScroll()
+
+  return (
+    <header
+      className={cn(
+        'sticky top-0 z-50 h-16 w-full bg-transparent',
+        isScrolled
+          ? 'shadow-sm backdrop-blur-[10px] duration-300 ease-in-out'
+          : '',
+      )}
+    >
+      <div className={cn('container h-full')}>
+        <div className={cn('flex h-full items-center justify-between')}>
+          <Link href="/" className={cn('text-2xl font-bold')}>
+            SlideTube - AI
+          </Link>
+          <ThemeSwitch />
+        </div>
+      </div>
+    </header>
+  )
+}
+
+export default Header
